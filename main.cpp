6030e5d6db55ec7106132424575c5d16a40aa834
Anna <glavatskikhas@gmail.com>
2024-12-01 00:24:54 +0300

    новые изменения в мейне

--- main.cpp
+++ main.cpp
@@ -36,7 +36,7 @@ int main() {
     std::cerr << "Not all tests passed! " << num_failed_tests << " failures.\n";
     return -1;
   } else {
-    std::cout << "Tests passed!" << std::endl;
+    std::cout << "passed!!!" << std::endl;
     return 0;
   }
 }
